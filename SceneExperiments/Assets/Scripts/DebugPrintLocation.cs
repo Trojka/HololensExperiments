7d75cbd16d097c22b2f143964bb033356e3a00ae
Serge Desmedt <sergedesmedt@yahoo.com>
2018-01-21 18:48:14 +0100

    SceneExperiments updates

---
+++ SceneExperiments/Assets/Scripts/DebugPrintLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPrintLocation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = this.transform.position;
+        Debug.Log("The gameobject is at location " + pos.ToString());
+	}
+}
